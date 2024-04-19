2e91a74db278974504fd16df1db78c868f85b4a8
semdepem <88699@glr.nl>
2024-04-19 12:44:57 +0200

    mini changes

--- php/muziekplayer.php
+++ php/muziekplayer.php
@@ -1,5 +1,7 @@
 <?php
 include '../inc/config.php';
+
+
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +10,7 @@ include '../inc/config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/muziekplayer.css">
-   
+    <script src="../javascript/muziekload.js"></script>
     <title>Jukebox</title>
 </head>
 <body>
@@ -16,7 +18,7 @@ include '../inc/config.php';
         <button class="cool">Login</button>
     </nav>
     <div class="parent">
-        <div class="div1" onclick="playMusic()"> RICK ROLL</div>
+        <div class="div1"> RICK ROLL</div>
         <div class="div2">2 </div>
         <div class="div3">3</div>
         <div class="div4"> 4</div>
@@ -28,31 +30,7 @@ include '../inc/config.php';
         <div class="div10">10 </div>
         <div class="div11">11 </div>
         <div class="div12">12 </div>
-        <div class="div13">LIJST </div>
+        <div class="div13">:LIJSt</div>
     </div>  
-
-    <script>
-        function playMusic() {
-            // Make an AJAX request to your server to fetch the MP3 file
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', '../php/muziekfetch.php', true); // Update path to your PHP script
-            xhr.responseType = 'blob';
-
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    var blob = xhr.response;
-                    var url = URL.createObjectURL(blob);
-
-                    // Create an audio element and set its source to the fetched MP3 file
-                    var audio = new Audio(url);
-
-                    // Play the audio
-                    audio.play();
-                }
-            };
-
-            xhr.send();
-        }
-    </script>
 </body>
 </html>
